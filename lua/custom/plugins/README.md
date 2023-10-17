b6b7a12168cc74e4f1f40c28fae5aa40507c6e56
Miguel Lavalle <mlavalle@redhat.com>
2023-10-17 15:47:29 -0500

    Initial commitment
    1) Enabled plugin installation from 'custom.plugins'
    2) Set up a few vim options from 'custom.plugins'

---
+++ lua/custom/plugins/README.md
@@ -0,0 +1,4 @@
+## Reset environment
+
+1. rm -rf ~/.config/nvim
+2. rm -rf ~/.local/share/nvim
---
+++ lua/custom/plugins/options.lua
@@ -0,0 +1,5 @@
+vim.wo.relativenumber = true
+vim.wo.colorcolumn = '80'
+-- This file is expected to return a plugin spec to be loades by lazy.nvim. But
+-- here we only want to set options, so we return an empty spec
+return {}
