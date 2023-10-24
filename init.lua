81c109987773306303975d5f50830fd9e273ddd6
Miguel Lavalle <mlavalle@redhat.com>
2023-10-24 11:12:32 -0500

    Add .editorconfig
    Added a .editorconfig file for Python and Go. Commented out
    tpope/vim-sleuth since nvim support .editorconfig out of the box

--- init.lua
+++ init.lua
@@ -71,8 +71,9 @@ require('lazy').setup({
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
 
-  -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-sleuth',
+  -- Detect tabstop and shiftwidth automatically. Commented out since neovim supports
+  -- .editorconfig out of the box
+  --'tpope/vim-sleuth',
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
