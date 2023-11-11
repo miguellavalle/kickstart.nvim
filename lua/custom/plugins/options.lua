63c5261563d1546ceae3407d75f31e2ae9f21aaf
Miguel Lavalle <mlavalle@redhat.com>
2023-11-11 12:23:29 -0600

    Add code folding and relative paths
    1) Enabled the folding expression with Treesitter
    2) Don't configure foldcolumn in nvim
    3) Configured relative path in lualine

--- lua/custom/plugins/options.lua
+++ lua/custom/plugins/options.lua
@@ -1,6 +1,10 @@
 vim.wo.relativenumber = true
-vim.wo.colorcolumn = '80'
-vim.wo.foldcolumn = '1'
--- This file is expected to return a plugin spec to be loades by lazy.nvim. But
+vim.opt.shada = "!,'30,<50,s10,h"
+
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.opt.foldlevelstart = 0
+
+-- This file is expected to return a plugin spec to be loaded by lazy.nvim. But
 -- here we only want to set options, so we return an empty spec
 return {}
